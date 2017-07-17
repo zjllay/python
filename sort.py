c6894b533ab9b80f692a85f068f073160d6d7154
zjllay <13501263471@163.com>
2017-07-17 15:40:13 +0800

    modify1

---
+++ sort.py
@@ -0,0 +1,11 @@
+def function(arr):
+    for i in range(len(arr)-1,0,-1):
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+    return arr
+arr = [54,26,94,43,31,44]
+
+print(function(arr))
